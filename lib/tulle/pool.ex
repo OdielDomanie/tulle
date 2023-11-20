21c9d264aba24f98ab43142f5bc16d2c7186d5a6
meozk <git@meozk.dev>
2023-11-20 22:18:48 +0300

    Write Pool

--- lib/tulle/pool.ex
+++ lib/tulle/pool.ex
@@ -1,88 +1,160 @@
-# defmodule Tulle.Pool do
-#   @moduledoc """
-#   Pool used for Http1.
-#   """
+defmodule Tulle.Pool do
+  @moduledoc """
+  Pool used for Http1.
+  """
 
-#   use GenServer
+  use GenServer
 
-#   import Map
+  require Logger
+  alias Tulle.Http1
 
-#   @worker_idle_max 60_000
+  @worker_idle_max 60_000
 
-#   @type t :: GenServer.name()
-#   @type worker :: pid
+  @type t :: GenServer.name()
+  @type worker :: pid
 
-#   @spec get_worker(t, (worker -> result)) :: result when result: any
-#   @doc """
-#   Do work in the pool.
+  def start_link(opts) do
+    connect_args =
+      Enum.filter(opts, fn {k, _} -> k in [:scheme, :address, :port, :connect_opts] end)
 
-#   A worker is checked-out if available.
-#   If non available, a new one is created.
-#   """
-#   def get_worker(pool, work) do
-#     case GenServer.call(pool, :get_worker) do
-#       {:available, worker} ->
-#         try do
-#           work.(worker)
-#         after
-#           check_in(pool, worker)
-#         end
+    GenServer.start_link(__MODULE__, {opts[:sv], connect_args}, opts)
+  end
 
-#       {:new, worker, worker_initer} ->
-#         try do
-#           with {:ok, worker} <- worker_initer.(worker) do
-#             work.(worker)
-#           end
-#         after
-#           check_in(pool, worker)
-#         end
-#     end
-#   end
+  @spec check_out!(t, timeout()) :: worker()
+  @doc """
+  Check-out a `Tulle.Http1.Client` process from the pool,
+  spawning if necessary.
+  """
+  def check_out!(pool, timeout \\ 5000) do
+    {:ok, client} = GenServer.call(pool, :check_out, timeout)
+    client
+  end
 
-#   @spec check_in(t, worker) :: :ok
-#   defp check_in(pool, worker), do: GenServer.cast(pool, {:check_in, worker})
+  @spec check_in(t, worker()) :: :ok
+  @doc """
+  Returns a checked-out `Tulle.Http1.Client` back to the pool.
+  """
+  def check_in(pool, client) do
+    GenServer.call(pool, {:check_in, client})
+  end
 
-#   def start_link({worker_child_spec, dyn_sv, opts}) do
-#     GenServer.start_link(__MODULE__, {worker_child_spec, dyn_sv}, opts)
-#   end
+  @impl true
+  def init({sv, connect_args}) do
+    {:ok, first_worker, timer} = spawn_http1_worker(sv, connect_args)
 
-#   @impl GenServer
-#   def init({worker_child_spec, dyn_sv}) do
-#     {:ok,
-#      %{
-#        wcs: worker_child_spec,
-#        sv: dyn_sv,
-#        stop_seq: true,
-#        workers: [],
-#        idle_timers: MapSet.new()
-#      }, {:continue, :start_first}}
-#   end
+    state = %{
+      workers: %{first_worker => timer},
+      seq: false
+    }
 
-#   @impl GenServer
-#   def handle_continue(:start_first, state) do
-#     [:ok, child | _] = DynamicSupervisor.start_child(state.sv, state.wcs) |> Tuple.to_list()
+    {:ok, state}
+  end
 
-#     Process.monitor(child)
-#     timer = Process.send_after(self(), @worker_idle_max, :worker_idle_timeout)
-#     state = %{state | workers: [child], idle_timers: MapSet.put(state.idle_timers, timer)}
+  defp spawn_http1_worker(sv, connect_args) do
+    spec =
+      Supervisor.child_spec(
+        {Http1, connect_args},
+        restart: :temporary
+      )
 
-#     {:noreply, state}
-#   end
+    with {:ok, worker} <- DynamicSupervisor.start_child(sv, spec) do
+      timer = set_timer(worker)
+      Process.monitor(worker)
+      {worker, timer}
+    end
+  end
 
-#   @impl GenServer
-#   def handle_info(
-#         :worker_idle_timeour,
-#         %{workers: [worker | workers_rest]} = state
-#       ) do
-#     case state.stop_seq do
-#       true ->
-#         DynamicSupervisor.terminate_child(state.sv, worker)
-#         {:noreply, %{state | workers: workers_rest, idle_timers: timers_rest, stop_seq: false}}
-#         false
+  defp set_timer(worker) do
+    Process.send_after(self(), {:timer, worker}, @worker_idle_max)
+  end
 
-#       false ->
-#         timer = Process.send_after(self(), @worker_idle_max, :worker_idle_timeout)
-#         {:noreply, %{state | idle_timers: [timer | timers_rest], stop_seq: true}}
-#     end
-#   end
-# end
+  @impl true
+  def handle_call(:check_out, _from, %{workers: workers} = state) when map_size(workers) > 0 do
+    [{worker, timer}] = Enum.take(workers, 1)
+    workers = Map.delete(workers, worker)
+    Process.cancel_timer(timer, async: true)
+    state = %{state | workers: workers}
+
+    {:reply, worker, state, {:continue, :spawn_if_low}}
+  end
+
+  @impl true
+  def handle_call(:check_out, _from, %{workers: workers} = state) when map_size(workers) == 0 do
+    case spawn_http1_worker(state.sv, state.connect_args) do
+      {:ok, new_worker, _new_timer} ->
+        {:reply, {:ok, new_worker}, state, {:continue, :spawn_if_low}}
+
+      error ->
+        {:reply, {:error, error}, state}
+    end
+  end
+
+  @impl true
+  def handle_call({:check_in, worker}, from, state) do
+    GenServer.reply(from, :ok)
+
+    timer = set_timer(worker)
+    workers = Map.put(state.workers, worker, timer)
+    state = %{state | workers: workers}
+    {:noreply, worker, state}
+  end
+
+  @impl true
+  def handle_info({:DOWN, _ref, :process, proc, _reason}, %{workers: workers} = state)
+      when is_map_key(workers, proc) do
+    {timer, workers} = Map.pop!(workers, proc)
+    Process.cancel_timer(timer, async: true)
+
+    state = %{state | workers: workers}
+    {:noreply, state, {:continue, :spawn_if_low}}
+  end
+
+  def handle_info({:DOWN, _ref, :process, proc, _reason}, %{workers: workers} = state)
+      when not is_map_key(workers, proc) do
+    {:noreply, state, {:continue, :spawn_if_low}}
+  end
+
+  def handle_info({:timer, worker}, %{workers: workers} = state)
+      when is_map_key(workers, worker) do
+    {_timer, workers} = Map.pop!(workers, worker)
+
+    workers =
+      if map_size(workers) > 0 and state.seq do
+        Process.exit(worker, :shutdown)
+        workers
+      else
+        timer = set_timer(worker)
+        Map.put(workers, worker, timer)
+      end
+
+    # Only stop the processes 1/2 of the time
+    # to smooth out the exits.
+    seq = not state.seq
+
+    state = %{state | workers: workers, seq: seq}
+    {:noreply, state, {:continue, :spawn_if_low}}
+  end
+
+  def handle_info({:timer, worker}, %{workers: workers} = state)
+      when not is_map_key(workers, worker) do
+    {:noreply, state}
+  end
+
+  @impl true
+  def handle_continue(:spawn_if_low, %{workers: workers} = state) when map_size(workers) >= 1 do
+    {:noreply, state}
+  end
+
+  def handle_continue(:spawn_if_low, %{workers: workers} = state) when map_size(workers) < 1 do
+    case spawn_http1_worker(state.sv, state.connect_args) do
+      {:ok, worker, timer} ->
+        workers = Map.put(workers, worker, timer)
+        state = %{state | workers: workers}
+        {:noreply, state}
+
+      other ->
+        Logger.warning(cant_spawn_worker: other)
+        {:noreply, state}
+    end
+  end
+end
---
+++ todo.md
@@ -0,0 +1,1 @@
+* Test `Tulle.Pool`
